78547c9d2ec120c7e499ae5aff0e3037d05311aa
MatthewMadore902 <90638212+MatthewMadore902@users.noreply.github.com>
2021-10-12 10:09:49 -0300

    added arrays

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -16,13 +16,15 @@ namespace Arrays
 			//shotgun 20/20
 			//.....
 
-			int weapon = 0;
+			int weapon = 2;
+			Console.WriteLine();
 
 
 
 			Console.WriteLine("Before Arrays");
 			int pistolAmmo = 6;
 			int shotGunAmmo = 20;
+			int lazer = 50;
 			//.....
 
 			// how do we show ammo for the current weapon
@@ -35,10 +37,57 @@ namespace Arrays
 			if (weapon == 1)
 			{
 				Console.WriteLine("ShutGun Ammo: " + shotGunAmmo);
+			}			
+			if (weapon == 2)
+			{
+				Console.WriteLine("lazer Ammo: " + lazer);
 			}
 
-			Console.WriteLine("Arrays");
-			
+			Console.WriteLine();
+
+			Console.WriteLine("With Arrays");
+
+			int lives = 3;
+
+			int[] ammo; //Declaring what ammo is.
+						// int[] means a array of integers.
+						//ammo is a array of integers.
+
+			int[] maxAmmo;
+			maxAmmo[0] = 6;
+
+			ammo = new int[3]; // initialization, telling compiler the size
+			ammo[0] = 6;
+			ammo[1] = 2;
+			ammo[2] = 50;
+
+			string[] weaponName = new string[3];
+			weaponName[0] = "pistol";
+			weaponName[1] = "shotgun";
+			weaponName[2] = "lazer";
+
+			//How do we show ammo wihout hard coding
+
+			/*			Console.WriteLine("Pistol Ammo: " + ammo[0]);
+						Console.WriteLine("ShutGun Ammo: " + ammo[1]);
+						Console.WriteLine("Lazer Ammo: " + ammo[2]);*/
+
+			if (weapon == 0)
+			{
+				Console.WriteLine(weaponName[weapon] + " Ammo: " + ammo[0]);
+			}
+			if (weapon == 1)
+			{
+				Console.WriteLine(weaponName[weapon] + " Ammo: " + ammo[1]);
+			}
+			if (weapon == 2)
+			{
+				Console.WriteLine(weaponName[weapon] + " Ammo: " + ammo[2]);
+			}
+
+
+
+
 			Console.ReadKey();
 		}
 	}
