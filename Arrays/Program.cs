98326d25d972e855d98f42b14a401e4cea96a230
MatthewMadore902 <90638212+MatthewMadore902@users.noreply.github.com>
2021-10-12 09:37:28 -0300

    showed coding before arrays

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -10,6 +10,36 @@ namespace Arrays
 	{
 		static void Main(string[] args)
 		{
+			Console.WriteLine("ammo System");
+			//We wish to store ammo of all weapons
+			//pistole = 6/6
+			//shotgun 20/20
+			//.....
+
+			int weapon = 0;
+
+
+
+			Console.WriteLine("Before Arrays");
+			int pistolAmmo = 6;
+			int shotGunAmmo = 20;
+			//.....
+
+			// how do we show ammo for the current weapon
+			//without hard coding
+
+			if (weapon == 0)
+			{
+				Console.WriteLine("Pistol Ammo: " + pistolAmmo);
+			}
+			if (weapon == 1)
+			{
+				Console.WriteLine("ShutGun Ammo: " + shotGunAmmo);
+			}
+
+			Console.WriteLine("Arrays");
+			
+			Console.ReadKey();
 		}
 	}
 }
